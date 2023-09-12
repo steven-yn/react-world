d241d4debfd84f24e9384bbadeea22ceff90a031
steven-yn <ssungyeon94@gmail.com>
2023-09-13 02:31:28 +0900

    chore: 스티치 셋업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import './globals.css';
+import { getCssText } from '../../stitches.config';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { styled } from '@stitches/react';
+
+const HomePageMain = styled('main', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  width: '100vw',
+  backgroundColor: 'lightgray',
+});
+
 export default function Home() {
-  return (
-    <main className="mb-3 mt-2 flex bg-red-400  text-blue-500">
-      홈 페이지 입니다만
-      <div className="mb-3 mt-2 flex bg-red-400  text-blue-500">
-        머냐 왜 안디냐
-      </div>
-    </main>
-  );
+  return <HomePageMain>스티치 테스트</HomePageMain>;
 }
